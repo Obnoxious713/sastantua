1c81ee07f707cb69cd0cdc2930cea56061a071ce
Josh Fleischer <joshcf11@gmail.com>
2017-12-02 18:26:45 -0500

    tryed to figure it out in python visualizer

---
+++ test.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+void        ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void        ft_putstr(char *str)
+{
+    while (*str != '\0')
+    {
+        ft_putchar(str);
+        str++;
+    }
+}
+
+void        findsquare(int size)
+{
+    int        x;
+    int        y;
+    int        tier_count;
+    int        tier_rnbr;
+    int        spaces;
+    int        tier_start;
+    int         col;
+    int         rows;
+
+    x = 0;
+    y = 0;
+    tier_start = 1;
+    tier_count = 1;
+    tier_rnbr = 1;
+    // the two outermost chars are '/' and '\' respectivly
+        // find the longest row based on the size
+            // subtract the amount in that row
+                // divide new number, spaces, by two
+                    // put that many spaces before and after '/' and '\'
+    while (tier_count <= size)
+    {
+        if (tier_count == 1)
+        {
+            x = 7;
+            y = 3;
+        }
+        
+        while (tier_rnbr <= y)
+        {
+            col = 0;
+            if (tier_rnbr > 1)
+            {
+              x + 2;
+              y + 2;
+            }
+            while (col <= x)
+            {
+              
+                spaces = (x - y) / 2;
+                if (col < spaces)
+                    ft_putchar(' ');
+                else if (col == spaces + 1)
+                    ft_putchar('/');
+                else if ((col > spaces) && (col < x - spaces))
+                  ft_putchar('*');
+                else if (col == x - spaces)
+                    ft_putchar('\\');
+                else if (col > x - spaces)
+                    ft_putchar(' ');
+            
+              if (col == x + 1)
+                {
+                  x = x + 12;
+                  y = y + 10;
+                  if (tier_count == tier_start + 1)
+                  {
+                      x + 2;
+                      y + 2;
+                      if (tier_count == tier_start + 2)
+                      {
+                          x + 2;
+                          x + 2;
+                      }
+                  }
+                  tier_start = tier_count;
+                 }
+              col++;
+            }
+          ft_putchar('\n');
+          tier_rnbr++;
+        }
+        tier_count++;
+    }
+}
+
+int   main()
+{
+    findsquare(1);
+    return (0);
+}
